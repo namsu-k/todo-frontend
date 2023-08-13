797bd273c3b29ded9cd6a55bc77fbc64df08ea2b
namsu-k <nghokr1111@gmail.com>
2023-08-13 23:00:42 +0900

    router setup

---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,16 @@
+import { Button, Heading, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <VStack justifyContent={"center"} minH={"100vh"}>
+      <Heading>Page not found</Heading>
+      <Text>It seems that you are lost.</Text>
+      <Link to="/">
+        <Button colorScheme="green" mt={4} variant={"link"}>
+          Go home &rarr;
+        </Button>
+      </Link>
+    </VStack>
+  );
+}
