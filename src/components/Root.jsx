6950918292e96d755b8a1a612ddd1aebe851f390
namsu-k <nghokr1111@gmail.com>
2023-08-15 00:43:35 +0900

    Header

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,9 +1,25 @@
+import { Box, Button, HStack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { FaCheckToSlot } from "react-icons/fa6";
 
 export default function Root() {
   return (
-    <h1>
+    <Box>
+      <HStack
+        py={5}
+        px={10}
+        borderBottomWidth={1}
+        justifyContent={"space-between"}
+      >
+        <Box color={"green.500"}>
+          <FaCheckToSlot size={48} />
+        </Box>
+        <HStack spacing={"2"}>
+          <Button>Log in</Button>
+          <Button colorScheme="green">Sign up</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-    </h1>
+    </Box>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -5,13 +5,7 @@ const config = {
   useSystemColorMode: false,
 };
 
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
+const colors = {};
 
 const theme = extendTheme({ config, colors });
 
